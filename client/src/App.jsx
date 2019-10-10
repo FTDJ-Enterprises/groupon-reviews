e4d728b60958f4804ca27b4835bc9759c617d23f
Jeehyae dance <jeehyae91@gmail.com>
2019-10-09 19:27:03 -0700

    setting up basic structure for the review service

---
+++ client/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class App extends React.Component {
+    constructor() {
+        super() 
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <p>Hi You</p>
+            </div>
+        )
+    }
+}
+
+export default App;
